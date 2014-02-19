8277f18739c01a58c3e3fed5ca2e74072c2b5ec4
David O' Rojo <david.orojo@gmail.com>
2014-02-19 15:49:29 -0600

    Removed solutions to exercises.

--- 1 Contenido/Ejercicios/01 Números/01_horas_en_un_año.rb
+++
@@ -1,9 +0,0 @@
-
-# encoding: UTF-8
-
-# Escribe un programa que te diga: ¿Cuántas horas hay en un año?
-
-# Un año tiene 365 días
-# Un día tiene 24 horas
-# Entonces: 1 año * 365 días * 24 horas
-puts 365 * 24
